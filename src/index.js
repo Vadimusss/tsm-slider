b85d12e8d456f070e2eb77d5f5d70d1c2eb03a97
Vadim <v_a_d@inbox.ru>
2020-11-10 21:54:03 +0300

    Added data for development

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-// @ts-check
+import React from 'react';
+import ReactDOM from 'react-dom'
+import images from './images.js';
+import Slider from './components/Slider.jsx';
 
-import init from './init.js';
+const sliderClass = '.point';
 
-init();
+const container = document.querySelector(sliderClass);
+const slides = {
+  imagesUrl: [
+    'https://www.tsm-company.ru/assets/templates/TSM-COMPANY.RU/img/slider_design_fon.jpg',
+    'https://www.tsm-company.ru/assets/templates/TSM-COMPANY.RU/img/slider_service_fon.jpg',
+  ], 
+}
+
+ReactDOM.render(<Slider slides={slides} container={container}/>, container);
