7cfb7a8b325b674ebf62e8eec952fb0eba93b392
Vadim <v_a_d@inbox.ru>
2020-11-26 22:04:23 +0300

    added dev data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import images from './images.js';
-import Slider from './components/Slider.jsx';
+import SliderInit from './SliderInit.jsx';
 
-const sliderClass = '.point';
+// eslint-disable-next-line no-undef
 
-const container = document.querySelector(sliderClass);
-const slides = {
-  imagesUrl: [
-    'https://www.tsm-company.ru/assets/templates/TSM-COMPANY.RU/img/slider_design_fon.jpg',
-    'https://www.tsm-company.ru/assets/templates/TSM-COMPANY.RU/img/slider_service_fon.jpg',
-  ], 
-}
+const sliders = [
+  {
+    type: 'slider',
+    containerClass: 'point',
+    autoPlay: false,
+    withDots: false,
+    slides: [
+      {
+        id: 1,
+        slideImgUrl: 'https://www.tsm-company.ru/assets/templates/TSM-COMPANY.RU/img/slider_design_fon.jpg',
+        slideTitle: 'title text',
+        slideDescription: 'description text',
+        slideLinkUrl: 'link URL',
+      },
+      {
+        id: 2,
+        slideImgUrl: 'https://www.tsm-company.ru/assets/templates/TSM-COMPANY.RU/img/slider_service_fon.jpg',
+        slideTitle: null,
+        slideDescription: null,
+        slideLinkUrl: null,
+      },
+      {
+        id: 3,
+        slideImgUrl: 'https://www.tsm-company.ru/assets/templates/TSM-COMPANY.RU/img/slider-design_fon02.jpg',
+        slideTitle: 'title text',
+        slideDescription: 'description text',
+        slideLinkUrl: 'link URL',
+      },
+    ],
+  },
+];
 
-ReactDOM.render(<Slider slides={slides} container={container}/>, container);
+sliders.forEach((slider) => {
+  const { type } = slider;
+  if (type === 'slider') {
+    SliderInit(slider);
+  }
+});
