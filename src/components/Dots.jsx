2f3e53bb2940d3114581ee1e1e584692ce58a76a
Vadim <v_a_d@inbox.ru>
2020-11-26 21:52:17 +0300

    added Dots component

---
+++ src/components/Dots.jsx
@@ -0,0 +1,35 @@
+/** @jsx jsx */
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { css, jsx } from '@emotion/core';
+
+const Dot = ({ active }) => (
+  <span
+    css={css`
+      padding: 8px;
+      margin-right: 5px;
+      cursor: pointer;
+      border-radius: 50%;
+      background: ${active ? '#006eae' : '#08a0d4'};
+    `}
+  />
+);
+
+const Dots = ({ slides, activeSlide }) => (
+  <div
+    css={css`
+      position: absolute;
+      bottom: 25px;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `}
+  >
+    {slides.map(({ id }, i) => (
+      <Dot key={id} active={activeSlide === i} />
+    ))}
+  </div>
+);
+
+export default Dots;
