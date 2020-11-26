e8573416776228124cf437226fa413896f49d840
Vadim <v_a_d@inbox.ru>
2020-11-26 21:53:37 +0300

    added slide overlay

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,18 +1,31 @@
 /** @jsx jsx */
-import React from 'react'
-import { css, jsx } from '@emotion/core'
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { css, jsx } from '@emotion/core';
 
-const Slide = ({ imageUrl }) => (
+const Slide = ({
+  width, containerClass, slide: {
+    slideImgUrl,
+    slideTitle,
+    slideDescription,
+    slideLinkUrl,
+  },
+}) => (
   <div
+    className={`${containerClass}__slide`}
     css={css`
       height: 100%;
-      width: 100%;
-      background-image: url('${imageUrl}');
+      width: ${width}px;
+      background-image: url('${slideImgUrl}');
       background-size: cover;
       background-repeat: no-repeat;
       background-position: center;
     `}
-  />
-)
+  >
+    {slideTitle && <span className={`${containerClass}__slide-title`}>{slideTitle}</span>}
+    {slideDescription && <span className={`${containerClass}__slide-description`}>{slideDescription}</span>}
+    {slideLinkUrl && <a href={slideLinkUrl} className={`${containerClass}__slide-link`}>подробнее об услуге</a>}
+  </div>
+);
 
-export default Slide
+export default Slide;
