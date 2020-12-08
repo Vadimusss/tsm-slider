a3cd2455b48823698acd93799b45e0f9713e72cc
Vadim <v_a_d@inbox.ru>
2020-12-08 21:17:28 +0300

    added test data

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import SliderInit from './SliderInit.jsx';
-
-// eslint-disable-next-line no-undef
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable no-undef */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Slider from './components/Slider.jsx';
 
 const sliders = [
   {
     type: 'slider',
     containerClass: 'point',
     autoPlay: false,
-    withDots: false,
+    withDots: true,
     slides: [
       {
         id: 1,
@@ -34,9 +36,62 @@ const sliders = [
   },
 ];
 
+const prepareSlides = (slides) => {
+  const quantity = slides.length;
+  if (quantity === 1) {
+    return [...slides, ...slides, ...slides].map((slide, key) => ({ ...slide, key }));
+  } if (quantity === 2) {
+    return [...slides, ...slides].map((slide, key) => ({ ...slide, key }));
+  }
+  return slides.map((slide, key) => ({ ...slide, key }));
+};
+
 sliders.forEach((slider) => {
   const { type } = slider;
   if (type === 'slider') {
-    SliderInit(slider);
+    const {
+      slides, containerClass, autoPlay, withDots,
+    } = slider;
+    // eslint-disable-next-line no-undef
+    const container = document.querySelector(`.${containerClass}`);
+    ReactDOM.render(<Slider
+      slides={prepareSlides(slides)}
+      containerClass={containerClass}
+      container={container}
+      autoPlay={autoPlay}
+      withDots={withDots}
+    />, container);
   }
 });
+
+/* const uri = document.location.pathname.substring(1);
+jQuery.ajax({
+  type: 'GET',
+  cache: false,
+  url: '/assets/get_fields.php',
+  data: { uri },
+  success(data) {
+    const sliders = JSON.parse(data);
+
+    sliders.forEach((slider) => {
+      const { type } = slider;
+      if (type === 'slider') {
+        const {
+          slides, containerClass, autoPlay, withDots,
+        } = slider;
+        // eslint-disable-next-line no-undef
+        const container = document.querySelector(`.${containerClass}`);
+        ReactDOM.render(<Slider
+          slides={prepareSlides(slides)}
+          containerClass={containerClass}
+          container={container}
+          autoPlay={autoPlay}
+          withDots={withDots}
+        />, container);
+      }
+    });
+  },
+  error(error) {
+    throw new Error(`Не удалось загрузить слайдеры! ${error.message}`);
+  },
+}); */
